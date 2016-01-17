427390177e1180ac5ae58865d47908dea38b99a2
Hadi Pribadi <kang.hadijoe@gmail.com>
2016-01-17 16:38:14 +0700

    Commit Pertama Kali

---
+++ WebTestGit/WebTestGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTestGit.Startup))]
+namespace WebTestGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
